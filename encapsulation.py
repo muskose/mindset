bdead662e1cb9a5677a77752aa09901fec0ecd77
Mustafa KÖSE <mustafa.kose@metglobal.com>
2020-10-18 01:52:22 +0300

    encapsulation updated

--- encapsulation.py
+++ encapsulation.py
@@ -1,1 +1,12 @@
-import keyword
+class Employee:
+    __version = 'v3.11.2'
+
+    def __init__(self, name, salary):
+        self._name = name
+        self.__salary = salary
+
+
+em = Employee('Ali Veli', 3500)
+print(em._name)
+print(em._Employee__version)
+print(em._Employee__salary)
